5630299916034698661c60fa8f749692b9382060
scarfada <83604371+scarfada@users.noreply.github.com>
2022-10-04 15:44:59 -0300

    usando props avançado

---
+++ src/components/Item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Alunos({ aluno, idade }) {
+  return (
+    <div>
+      <li>
+        {aluno}
+        {idade}
+      </li>
+    </div>
+  );
+}
--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Alunos from './Item';
 
 export default function List() {
   return (
     <>
-      <h2>Lista de Compras</h2>
+      <h2>Lista de Alunos</h2>
       <ul>
-        <li>Arroz</li>
-        <li>Feijão</li>
-        <li>Leite</li>
+        <Alunos aluno="Laura" idade={19} />
+        <Alunos aluno="Esmeraldo" idade={20} />
+        <Alunos aluno="Tim" idade={28} />
+        <Alunos aluno="Carmem" idade={26} />
+        <Alunos aluno="Alex" idade={19} />
+        <Alunos aluno="Car" idade={24} />
       </ul>
     </>
   );
